e1b2983892078d5aae735bb81b403bfacb22daa2
Dixie Cravens <dcravens@gmail.com>
2020-03-14 22:22:22 -0400

    last update

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 var express = require("express");
 var app = express();
-
+const path = require('path');
+const url = require('url');
 const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL || "postgres://catherine:jinx@localhost:5432/collection";
 const pool = new Pool({connectionString: connectionString});
 
 app.set("port", (process.env.PORT || 5000));
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended : true}));
+
 //endpoints
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
 // app.post("/logIn", logIn);
 app.get("/seeCollection", seeCollection);
 
+// app.get("/seeCollection", (req, res) => {
+   
+//     const collection = req.query.collection;
+//     const result = seeCollection(collection);
+//     const params = {collection: collection, result: result};
+
+//     res.render("showCollection", params);
+
+// });
 
 
 
@@ -22,32 +38,33 @@ app.listen(app.get("port"), function() {
 
 //functions
 
-function seeCollection(req, res) {
-    console.log("Working on request for the collection.");
 
-    var collection = req.query.collection;
-    console.log("Retrieving collection ", collection);
+function seeCollection(req, res) {
+    
+    const collection = req.query.collection;
+    
 
     seeCollectionFromDb(collection, function(error, result) {
         console.log("Back from the seeCollectionFromDb function with result: ", result);
-        
-        if (error) {
+
+        if (error || result == null) {
             res.status(500).json({success: false, data: error});
         }else {
-            res.json(result);
-            //add table structure here to display result
+            res.json(result); 
         }
-        
+           
     });
-    
+    const result = JSON.stringify(result);
+    const params = {collection: collection, result: result};
+    console.log("Retrieving collection ", collection);
+    res.render("showCollection", params);  
 }
 
 function seeCollectionFromDb(collection, callback) {
     console.log("GetCollectionFromDB called with collection: ", collection);
     
     var sql = "SELECT item_name, item_description FROM collections WHERE collection_name = $1::text";
-    // var sql = buildSql;
-    var params = [collection];
+    let params = [collection];
 
     pool.query(sql, params, function(err, result) {
         if (err) {
