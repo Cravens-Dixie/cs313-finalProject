af2c6ceb7ae27bb3e81ce102ef81dcce1182a51f
Dixie Cravens <dcravens@gmail.com>
2020-04-02 17:37:18 -0400

    removed debug code

--- models/userModels.js
+++ models/userModels.js
@@ -2,21 +2,17 @@
 const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
-
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
 function searchForUser(userName, userPassword, callback) {
     //validate a user and password
-    
     var sql = "SELECT username, password FROM collection_owners WHERE username = $1::text";
     var params = [userName];
-    console.log("Searching DB for matching username and password: " + userName + " " + userPassword)
 
     pool.query(sql, params, function(error, db_results) {
         if (error) {
-            //var db_results = {name: "dberror", message: "The database is not responding."};
             console.log("An error occurred with the DB" + error);
             callback(error, null);
         }else if (db_results.rows.length == 0) {
@@ -28,15 +24,11 @@ function searchForUser(userName, userPassword, callback) {
             var hash = db_results.rows[0].password;
             
             bcrypt.compare(enteredPassword, hash, function(error, result) {
-            
                 if (result == true) {
-                    //var result = ({user: userName});
-                    console.log ("Password is a match.")
                     callback(null, result);// returns results to userController.validateUser()
 
                 }else {
                     console.log("The password is not a match.");
-                    //var error = {name: "userErr", message:"Passwords did not match"};
                     callback(error, null);// returns error to userController.validateUser()
                     
                 }
@@ -48,9 +40,6 @@ function searchForUser(userName, userPassword, callback) {
     });
 };
  
-function passwordCompare(userPassword, hash) {
-
-}
 
 function insertNewUser(userName, password, callback) {
     //Create a new user and password
@@ -65,11 +54,8 @@ function insertNewUser(userName, password, callback) {
                 console.log(err);
                 callback(err, null);
             }else {
-                console.log("New User: " + userName + " inserted into DB");
-                console.log(db_results);
-
-                var results = {user:userName};
-                callback(null, results);
+                //var results = {user:userName};
+                callback(null, db_results);
             };
  
         });
